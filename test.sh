0aae83dd0141ffbc0ec5d7b671574be1b9b607c4
D. Bohdan <dbohdan@dbohdan.com>
2024-11-05 09:25:59 +0000

    test: add tests and improve test log output

--- test.sh
+++ test.sh
@@ -7,43 +7,53 @@ set -eu
 
 random_sleep=${RANDOM_SLEEP:-./random-sleep}
 
-test_arg() {
+test_args() {
     _cond=$1
-    _arg=$2
+    shift
 
-    _print_arg=$_arg
-    if [ -z "$_print_arg" ]; then
-        _print_arg="''"
+    if [ -z "$1" ]; then
+        _print_args="'' $*"
+    else
+        _print_args=$*
     fi
-    printf 'testing %-3s %s\n' "$_cond" "$_print_arg"
+    test_number=$((test_number + 1))
+    printf '%2d  expect %-3s  %-6s' "$test_number" "$_cond" "$_print_args"
 
     case "$_cond" in
     err)
-        if "$random_sleep" "$_arg" >/dev/null 2>&1; then
-            printf 'test failed\n'
+        if "$random_sleep" "$@" >/dev/null 2>&1; then
+            printf '  failed'
             status=1
         fi
         ;;
     ok)
-        if ! "$random_sleep" "$_arg" >/dev/null 2>&1; then
-            printf 'test failed\n'
+        if ! "$random_sleep" "$@" >/dev/null 2>&1; then
+            printf '  failed'
             status=1
         fi
         ;;
     *)
         printf 'invalid condition: "%s"\n' "$_cond"
-        exit 1
+        return 1
         ;;
     esac
+
+    printf '\n'
 }
 
 status=0
+test_number=0
 
-test_arg err ''
-test_arg err 0ms
-test_arg ok --help
-test_arg ok 0
-test_arg ok 0h
-RANDOM_SLEEP_DEBUG='' test_arg ok 2
+test_args err ''
+test_args err 0ms
+test_args err -1
+test_args err -v
+test_args err -x
+test_args ok --help
+test_args ok -V
+test_args ok -v 0
+test_args ok 0
+test_args ok 0h
+RANDOM_SLEEP_DEBUG='' test_args ok 2
 
-exit "$status"
+return "$status"
